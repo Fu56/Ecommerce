864b743aca27440a4ac33588fb8d1787d94047b0
Fu56 <fuadabdela95@gmail.com>
2025-12-22 10:43:19 +0300

    refactor Header component to include a responsive navigation bar with links

--- frontend/client/src/components/Layout/Header.jsx
+++ frontend/client/src/components/Layout/Header.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header=()=>{
     return(
         <>
-        <h1>Header Ecommerce website</h1>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo01"
+                        aria-controls="navbarTogglerDemo01"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <Link to="/" className="navbar-brand">
+                            🛒 E-commerce App
+                        </Link>
+
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/category" className="nav-link">
+                                    Category
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/register" className="nav-link">
+                                    Register
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/login" className="nav-link">
+                                    Login
+                                </NavLink>
+                            </li>
+                        </ul>
+
+                        {/* //! The search form is kept as is for now, but will likely need functionality later. */}
+                        {/* <form className="d-flex" role="search">
+                            <input
+                                className="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-outline-success" type="submit">
+                                Search
+                            </button>
+                        </form> */}
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
